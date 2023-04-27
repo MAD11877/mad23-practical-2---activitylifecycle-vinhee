178e25c9da50d51ae0967602694964c49ffe7497
vinhee <115960216+vinhee@users.noreply.github.com>
2023-04-27 19:08:27 +0800

    Adding files

---
+++ User.java
@@ -0,0 +1,46 @@
+public class User {
+
+    public String Name = "0";
+    public String Description = "0";
+    public int Id = 0;
+    public Boolean Followed = true;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public Boolean getFollowed() {
+        return Followed;
+    }
+
+    public void setFollowed(Boolean followed) {
+        Followed = followed;
+    }
+
+    public User(String name, String description, int id, Boolean followed) {
+        Name = name;
+        Description = description;
+        Id = id;
+        Followed = followed;
+    }
+}
